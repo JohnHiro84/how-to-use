5eb371d93516e1b658d15dd7bf93a5027660a076
Alexander Yambao <32828383+JohnHiro84@users.noreply.github.com>
2020-09-21 00:02:33 -0700

    Update slideshow.js

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -7,6 +7,14 @@ function Slideshow(){
 
    let imageString = (<img className="slideshow-image" src={require("./page" + (page).toString() + ".png")} alt="slideshow" />)
 
+  function setPageUpClickHandler(){
+    setPage((page < 4) ? (page + 1) : page);
+  }
+
+  function setPageDownClickHandler(){
+    setPage((page > 1) ? (page - 1) : page);
+  }
+
   return (
     <div>
       <button onClick="" >button</button>
